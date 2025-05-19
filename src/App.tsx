a8b5f0ef575672653b8de4855a811429d397aeec
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-20 00:36:47 +0200

    changed export

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {TodolistItem} from "./TodolistItem.tsx";
 
-function App() {
-  return (
-      <div className="app">
-        <TodolistItem/>
-      </div>
-  )
+export const App = () => {
+    return (
+        <div className="app">
+            <TodolistItem/>
+            <TodolistItem/>
+            <TodolistItem/>
+        </div>
+    )
 }
 
-export default App
+
