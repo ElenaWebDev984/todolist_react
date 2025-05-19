ce7b8df9141d3da11b5cc18f5c66bb1b273c14a4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-20 01:56:45 +0200

    added more tasks

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ export const App = () => {
         { id: 2, title: 'JS', isDone: true },
         { id: 3, title: 'ReactJS', isDone: false },
         { id: 4, title: 'Redux', isDone: false },
+        { id: 5, title: 'Typescript', isDone: false },
+        { id: 6, title: 'RTK query', isDone: false },
     ]
 
     const tasks2: Task[] = [
