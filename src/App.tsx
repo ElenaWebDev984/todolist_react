a18e7d0a0d8441593778ea0fa368018c103b2a57
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-20 16:05:59 +0200

    Revert "added idType"
    This reverts commit 19b9910ce33bf800a129a8031a73b230ccb8c927.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
 import {TodolistItem} from "./TodolistItem.tsx";
 import {useState} from "react";
-import {v1} from "uuid";
 
 export type Task = {
     title: string
-    id: string
+    id: number
     isDone: boolean
 }
 
@@ -16,15 +15,15 @@ export const App = () => {
     const [filter, setFilter] = useState<filterValues>('all');
 
     const [tasks, setTasks] = useState<Task[]>([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-        {id: v1(), title: 'Redux', isDone: false},
-        {id: v1(), title: 'Typescript', isDone: false},
-        {id: v1(), title: 'RTK query', isDone: false},
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTK query', isDone: false},
     ]);
 
-    const deleteTask = (taskId: string) => {
+    const deleteTask = (taskId: number) => {
         const filteredTasks = tasks.filter(task => {
             return task.id !== taskId
         })
@@ -42,8 +41,6 @@ export const App = () => {
     const changeFilter = (filter: filterValues) => {
         setFilter(filter)
     }
-
-
 
 
 
